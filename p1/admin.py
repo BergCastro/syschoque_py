f447a632ba684f3c3ddbbae84e86c4a4775938ce
Lindemberg Castro <fireberg2500@hotmail.com>
2018-11-10 23:53:03 -0300

    add Opm Cargo

--- p1/admin.py
+++ p1/admin.py
@@ -3,6 +3,18 @@ from django.forms import ModelForm, TextInput
 from .models import PolicialMilitar
 from .models import Oficio
 from .models import TipoOficio
+from .models import Opm
+from .models import Cargo
+
+
+class CargoAdmin(admin.ModelAdmin):
+    fields = [('nome', 'abreviacao')]
+    list_display = ('nome', 'abreviacao')
+
+class OpmAdmin(admin.ModelAdmin):
+    fields = ['nome']
+    list_display = ('id', 'nome')
+
 
 class OficioAdmin(admin.ModelAdmin):
     fields = [('data', 'data_missao', 'status'), ('tipo', 'assunto'), ('referencia', 'anexo'), 'destino', 'conteudo', 'arquivo' ]
@@ -15,14 +27,27 @@ class OficioAdmin(admin.ModelAdmin):
 class PolicialAdmin(admin.ModelAdmin):
     #fields = [('data', 'data_missao', 'status'), ('tipo', 'assunto'), ('referencia', 'anexo'), 'destino', 'conteudo' ]
     fieldsets = [
+        (None, {'fields': [('numero', 'matricula'),
+                           ('nome', 'nome_guerra'),
+                           ('cargo', 'opm')
 
-    ('Dados Pessoais',  {'fields': [('nome', 'nome_guerra'),
-                                        ('data_nascimento', 'sexo')]}),
+                          ]
+
+               }
+        ),
+
+        ('Dados Pessoais',  {'fields': [('data_nascimento', 'sexo'),
+                                        ('tipo_sanguineo', ),
+
+                                        ],
+                             'classes': ['collapse']
+                             }
+        ),
         ('Documentos', {'fields': ['cpf', 'rg'], 'classes': ['collapse']}),
     ]
-   #list_filter = ['status', 'tipo']
-   #list_display = ('nome', 'nome_guerra', 'cpf', 'rg')
-   #search_fields = ['numero', 'status', 'tipo__tipo']
+    list_filter = ['cargo__nome']
+    list_display = ('matricula', 'cargo','nome', 'nome_guerra', 'cpf', 'rg')
+    search_fields = ['numero', 'nome', 'matricula', 'cargo__abreviacao']
 
 
 
@@ -36,6 +61,8 @@ class TipoOficioAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(PolicialMilitar)
+admin.site.register(PolicialMilitar, PolicialAdmin)
 admin.site.register(Oficio, OficioAdmin)
 admin.site.register(TipoOficio, TipoOficioAdmin)
+admin.site.register(Cargo, CargoAdmin)
+admin.site.register(Opm, OpmAdmin)
--- p1/models.py
+++ p1/models.py
@@ -5,10 +5,67 @@ from django.db import models
 from tinymce.models import HTMLField
 
 
+class SituacaoFuncional(models.Model):
+    nome = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.upper()
+        super(SituacaoFuncional, self).save(*args, **kwargs)
+
+
+class Opm(models.Model):
+    nome = models.CharField(verbose_name='OPM',
+                            max_length=30
+                            )
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.upper()
+        super(Opm, self).save(*args, **kwargs)
+
+
+
+# CARGO
+class Cargo(models.Model):
+    nome = models.CharField(verbose_name='Cargo',
+                            max_length=30)
+    abreviacao = models.CharField(verbose_name='Abreviação',
+                                  max_length=10)
+    def __str__(self):
+        return self.abreviacao
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.upper()
+        self.abreviacao = self.abreviacao.upper()
+        super(Cargo, self).save(*args, **kwargs)
+
 
 class PolicialMilitar(models.Model):
-    nome = models.CharField(max_length=30)
-    nome_guerra = models.CharField(max_length=30)
+    matricula = models.CharField(verbose_name='Matrícula',
+                                 max_length=8,
+                                 default='00000000',
+                                 unique=True)
+    numero = models.CharField(verbose_name='Número',
+                              max_length=6,
+                              default=000000)
+    nome = models.CharField(verbose_name='Nome', max_length=30)
+    nome_guerra = models.CharField(verbose_name='Nome de Guerra', max_length=30)
+    cargo = models.ForeignKey(Cargo,
+                              on_delete=models.DO_NOTHING,
+                              verbose_name='Cargo',
+                              )
+    opm = models.ForeignKey(Opm,
+                            on_delete=models.DO_NOTHING,
+                            verbose_name='OPM',
+
+                            )
+    situacao_funcional = models.ForeignKey(SituacaoFuncional,
+                                           on_delete=models.DO_NOTHING,
+                                           verbose_name='Situação Funcional')
     data_nascimento = models.DateField(verbose_name='Data Nascimento')
     SEXO = (
         ('M', 'Masculino'),
@@ -40,10 +97,18 @@ class PolicialMilitar(models.Model):
     def __str__(self):
         return self.nome
 
+    def save(self, *args, **kwargs):
+        self.matricula = self.matricula.upper()
+        self.nome_guerra = self.nome_guerra.upper()
+        self.nome = self.nome.upper()
+        super(PolicialMilitar, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name_plural = "Policiais"
         verbose_name = 'Policial Militar'
         db_table = 'p1_policiais'
+
+
 
 
 
@@ -66,12 +131,16 @@ class TipoOficio(models.Model):
         verbose_name = 'Tipo de Ofício'
         db_table = 'p1_tipos_oficio'
 
+    def save(self, *args, **kwargs):
+        self.numero = '001'
+        super(Oficio, self).save(*args, **kwargs)
+
 
 
 
 
 class Oficio(models.Model):
-    numero = models.CharField(verbose_name='Número', max_length=10, default='0001/ghg')
+    numero = models.CharField(verbose_name='Número', max_length=10)
     STATUS = (
         ('Aberto', 'Aberto'),
         ('Arquivado', 'Arquivado'),
@@ -86,9 +155,8 @@ class Oficio(models.Model):
     )
     tipo = models.ForeignKey(TipoOficio,
                              verbose_name='Tipo',
-                             on_delete=models.DO_NOTHING,
-                             blank=True,
-                             null=True
+                             on_delete=models.SET_DEFAULT,
+                             default=0
                              )
     assunto = models.CharField(verbose_name='Assunto', max_length=30)
     referencia = models.CharField(verbose_name='Referência', max_length=30)
